8e14e69e7ae7530c36e60ee7e8cdfa225c28e346
NetAcad DEVASC <changeme@example.com>
2023-07-05 14:44:41 +0000

    EXAMEN DE PROGRAMACION!

---
+++ Integrantes.py
@@ -0,0 +1,5 @@
+integrantes = ["Javier Rojo", "Bryan Lagos", "Nicolas Rubilar", "Pablo Guerrero"]
+
+print("LOS INTEGRANTES DE ESTE EXAMEN SON:")
+for integrante in integrantes:
+    print(integrante)
